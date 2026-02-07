1bb392168bec85eb84984da474373616736165c6
Mufeed kcp <bitengineguy@gmail.com>
2026-02-07 22:18:35 +0530

    Update file_handlers.py

--- file_handlers.py
+++ file_handlers.py
@@ -15,21 +15,4 @@ file_handler = logging.FileHandler("report.log")
 file_handler.setLevel(logging.ERROR)
 logger.addHandler(file_handler)
 logger.error("this goes to console and file")
-=======
-import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-# Console handler
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-logger.addHandler(console_handler)
-logger.info("this goes to console")
-
-# File handler
-file_handler = logging.FileHandler("report.log")
-file_handler.setLevel(logging.ERROR)
-logger.addHandler(file_handler)
-logger.error("this goes to console and file")
->>>>>>> b4747ebf1130123dbce612e363906aebfa683496
